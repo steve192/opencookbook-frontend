801969297ef27f94a1b394bca1aa753493a8be52
steve192 <stefan@sterul.com>
2021-11-08 23:28:29 +0100

    Add terms and conditions to signup screen

--- src/screens/LoginScreen/SignupScreen.tsx
+++ src/screens/LoginScreen/SignupScreen.tsx
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
-import { Button, Card, Icon, Input, Layout, Modal, Text } from '@ui-kitten/components';
-import { ScrollView, View, StyleSheet, ImageBackground, KeyboardAvoidingView, Platform } from 'react-native';
-import RestAPI from '../../dao/RestAPI';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Button, CheckBox, Input, Text, useTheme } from '@ui-kitten/components';
+import React, { useState } from 'react';
+import { Linking, StyleSheet, View } from 'react-native';
+import Spacer from 'react-spacer';
+import RestAPI from '../../dao/RestAPI';
 import { MainNavigationProps } from '../../navigation/NavigationRoutes';
 import CentralStyles from '../../styles/CentralStyles';
-import Configuration from '../../Configuration';
-import Spacer from 'react-spacer';
-import { color } from 'react-native-reanimated';
 import { LoginBackdrop } from './LoginBackdrop';
 
 
@@ -20,6 +18,10 @@ export const SignupScreen = ({ route, navigation }: Props) => {
     const [password, setPassword] = useState<string>("");
     const [passwordConfirm, setPasswordConfirm] = useState<string>("");
     const [apiErrorMessage, setApiErrorMessage] = useState<string>();
+    const [termsAccepted, setTermsAccepted] = useState<boolean>(false);
+
+    const theme = useTheme();
+
 
     const register = () => {
 
@@ -39,19 +41,33 @@ export const SignupScreen = ({ route, navigation }: Props) => {
 
     return (
         <LoginBackdrop>
-                <View style={styles.loginContainer}>
-                    <View style={styles.innerLoginContainer}>
-                        <Text style={styles.title}>Register</Text>
-                        <Input value={email} onChangeText={text => setEmail(text)} placeholder="E-Mail"></Input>
-                        <Spacer height={20} />
-                        <Input status={passwordsMatching ? "basic" : "danger"} value={password} onChangeText={setPassword} placeholder="Password" secureTextEntry={true} />
-                        <Spacer height={10} />
-                        <Input status={passwordsMatching ? "basic" : "danger"} value={passwordConfirm} onChangeText={setPasswordConfirm} placeholder="Password Confirm" secureTextEntry={true} />
-                        {!passwordsMatching ? <Text status="danger">Passwords do not match</Text> : null }
-                        <Button disabled={allFieldsOk ? false: true} style={CentralStyles.elementSpacing} onPress={register}>Register</Button>
-                        {apiErrorMessage ? <Text status="danger">{apiErrorMessage}</Text> : null}
+            <View style={styles.loginContainer}>
+                <View style={styles.innerLoginContainer}>
+                    <Text style={styles.title}>Register</Text>
+                    <Input value={email} onChangeText={text => setEmail(text)} placeholder="E-Mail"></Input>
+                    <Spacer height={20} />
+                    <Input status={passwordsMatching ? "basic" : "danger"} value={password} onChangeText={setPassword} placeholder="Password" secureTextEntry={true} />
+                    <Spacer height={5} />
+                    <Input status={passwordsMatching ? "basic" : "danger"} value={passwordConfirm} onChangeText={setPasswordConfirm} placeholder="Password Confirm" secureTextEntry={true} />
+                    {!passwordsMatching ? <Text status="danger">Passwords do not match</Text> : null}
+                    <Spacer height={20} />
+                    <View style={{ flexDirection: "row" }}>
+                        <CheckBox checked={termsAccepted} onChange={setTermsAccepted} />
+                        <Text
+                            style={{ paddingLeft: 10, color: "white" }}>
+                            I accept the{" "}
+                            <Text
+                                onPress={() => Linking.openURL("https://google.com")}
+                                style={{ color: theme["color-primary-default"] }}>
+                                terms and conditions
+                            </Text>
+                        </Text>
                     </View>
+                    <Spacer height={20} />
+                    <Button disabled={allFieldsOk ? false : true} style={CentralStyles.elementSpacing} onPress={register}>Register</Button>
+                    {apiErrorMessage ? <Text status="danger">{apiErrorMessage}</Text> : null}
                 </View>
+            </View>
         </LoginBackdrop>
     )
 
@@ -75,5 +91,8 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+        marginLeft: 16,
+        marginRight: 16
     },
+
 });
