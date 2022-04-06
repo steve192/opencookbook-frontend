a502abce4dcf471affae2d894cb1ce565443cacb
steve192 <stefan@sterul.com>
2022-04-06 19:21:09 +0200

    remove ui kitten from app

--- App.tsx
+++ App.tsx
@@ -1,17 +1,11 @@
 /* eslint-disable react/display-name */
-import * as eva from '@eva-design/eva';
-import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
-import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import React from 'react';
 import {enableScreens} from 'react-native-screens';
 import {Provider, useSelector} from 'react-redux';
-import {default as customMapping} from './mapping.json';
 import {Prompt} from './src/helper/Prompt';
 import './src/i18n/config';
 import MainNavigation from './src/navigation/MainNavigation';
 import {RootState, store} from './src/redux/store';
-import {myDarkTheme} from './src/styles/custom-theme-dark';
-import {myLightTheme} from './src/styles/custom-theme-light';
 import {Colors, DarkTheme, DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 
 enableScreens();
@@ -58,16 +52,8 @@ const ReduxWrappedApp = () => {
   };
   return (
     <PaperProvider theme={selectedTheme === 'light' ? paperTheme : darkPaperTheme}>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider
-        {...eva}
-        // @ts-ignore
-        customMapping={customMapping}
-        theme={selectedTheme === 'light' ? myLightTheme : myDarkTheme}>
-
-        <MainNavigation />
-        <Prompt/>
-      </ApplicationProvider>
+      <MainNavigation />
+      <Prompt/>
     </PaperProvider>
   );
 };
