16deb0bb6298668c7ada4061a2b8b01afd44c797
steve192 <stefan@sterul.com>
2022-11-27 00:34:52 +0100

    feat: pinchable images (#113)
    * first draft
    
    * fix image offset
    
    * remove unused imports
    
    * restrict zoomable images to recipe screen
    
    * fix transaction manager by moving resture handler to memo
    
    * Fix pinching not working with images in pagerview
    
    * Refactor
    
    * Fix sonar issues

--- src/components/RecipeImageComponent.tsx
+++ src/components/RecipeImageComponent.tsx
@@ -1,26 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import {Image, Platform, StyleSheet, View} from 'react-native';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
+import {Animated, Easing, GestureResponderEvent, Image, NativeTouchEvent, PanResponder, PanResponderGestureState, Platform, StyleSheet, View} from 'react-native';
+import {Portal} from 'react-native-paper';
 import {fetchSingleImage, fetchSingleThumbnailImage} from '../redux/features/imagesSlice';
 import {useAppDispatch, useAppSelector} from '../redux/hooks';
+import {RootState} from '../redux/store';
 
 interface Props {
     uuid?: string
     forceFitScaling?: boolean
     useThumbnail?: boolean
     blurredMode?: boolean
+    zoomable?: boolean
 }
 export const RecipeImageComponent = (props: Props) => {
-  let imageData;
-  if (props.uuid) {
-    // @ts-ignore
-    if (props.useThumbnail) {
-      imageData = useAppSelector((state) => state.images.thumbnailImageMap[props.uuid]);
-    } else {
-      imageData = useAppSelector((state) => state.images.imageMap[props.uuid]);
-    }
-  }
+  const selector = !props.uuid ?
+      () => undefined :
+      props.useThumbnail ?
+      (state: RootState) => state.images.thumbnailImageMap[props.uuid!] :
+      (state: RootState) => state.images.imageMap[props.uuid!];
+
+  const imageData = useAppSelector(selector);
+
   const [requestPending, setRequestPending] = useState<boolean>(false);
   const dispatch = useAppDispatch();
+
+  const [isDragging, setIsDragging] = useState(false);
+
+  const gestureInProgress = useRef<number>();
+  const initialTouches = useRef<NativeTouchEvent[]>();
+  const initialImageSize = useRef<{width: number, height:number}>();
+  const imageRef = useRef<Image>(null);
+
+  const pinchImagePosition = useRef(new Animated.ValueXY());
+  const scaleValue = useRef(new Animated.Value(1));
 
 
   // Hook for loading images used and putting them in the buffer
@@ -44,21 +56,206 @@ export const RecipeImageComponent = (props: Props) => {
   // Blurring is stronger on web, compensate
   const blurAmount = Platform.OS === 'web' ? 2 : 10;
 
+  useEffect(() => {
+    Animated.timing(opacity.current, {
+      useNativeDriver: true,
+      toValue: isDragging ? 0 : 1,
+      duration: 200,
+    }).start();
+  }, [isDragging]);
+
+  const onStartGesture = async (event: GestureResponderEvent, gestureState: PanResponderGestureState ) => {
+    if (gestureInProgress.current) {
+      return;
+    }
+    event.preventDefault();
+    event.stopPropagation();
+
+    const {touches} = event.nativeEvent;
+
+    return new Promise<void>((resolve) => {
+      imageRef.current?.measure((x, y, width, height, pageX, pageY) => {
+        pinchImagePosition.current.setValue({x: 0, y: 0});
+        pinchImagePosition.current.setOffset({x: pageX, y: pageY});
+        initialImageSize.current = {width: width, height: height};
+        initialTouches.current = touches;
+        gestureInProgress.current = gestureState.stateID;
+        setIsDragging(true);
+        resolve();
+      });
+    });
+  };
+
+  const onGestureMove = (event: GestureResponderEvent, gestureState: PanResponderGestureState) => {
+    const {touches} = event.nativeEvent;
+    if (!gestureInProgress.current) {
+      return;
+    }
+
+    if (touches.length < 2) {
+      onGestureRelease(event, gestureState);
+      return;
+    }
+
+    const currentTouchPosition = getMiddleBetween2Touches(touches);
+    const initialTouchPosition = getMiddleBetween2Touches(initialTouches.current!);
+
+    const {x, y} = getDeltaTranslation(currentTouchPosition, initialTouchPosition);
+    pinchImagePosition.current.x.setValue(x);
+    pinchImagePosition.current.y.setValue(y);
+
+    const newScale = calculateScaleFromTouches(touches, initialTouches.current!);
+    scaleValue.current.setValue(Math.max(newScale, 1));
+  };
+
+  const onGestureRelease = (event: GestureResponderEvent, gestureState: PanResponderGestureState) => {
+    return new Promise<void>((resolve) => {
+      imageRef.current?.measure((x, y, width, height, pageX, pageY) => {
+        gestureInProgress.current = undefined;
+        initialTouches.current = [];
+
+        Animated.parallel([
+          Animated.timing(pinchImagePosition.current.x, {
+            useNativeDriver: true,
+            toValue: 0,
+            duration: 500,
+            easing: Easing.linear,
+          }),
+          Animated.timing(pinchImagePosition.current.y, {
+            useNativeDriver: true,
+            toValue: 0,
+            duration: 500,
+            easing: Easing.linear,
+          }),
+          Animated.timing(scaleValue.current, {
+            useNativeDriver: true,
+            toValue: 1,
+            duration: 500,
+            easing: Easing.linear,
+          }),
+        ]).start(() => {
+          pinchImagePosition.current.setOffset({
+            x: pageX,
+            y: pageY,
+          });
+          requestAnimationFrame(() => {
+            opacity.current.setValue(1);
+            setIsDragging(false);
+          });
+        });
+        resolve();
+      });
+    });
+  };
+
+  useEffect(() => {
+
+  }, []);
+
+
+  const gestureHandler = useMemo(() => {
+    return PanResponder.create({
+      onStartShouldSetPanResponderCapture: ({nativeEvent}) => {
+        return nativeEvent.touches.length === 2;
+      },
+      onMoveShouldSetPanResponderCapture: ({nativeEvent}) => {
+        return nativeEvent.touches.length === 2;
+      },
+      onPanResponderGrant: onStartGesture,
+      onPanResponderMove: onGestureMove,
+      onPanResponderRelease: onGestureMove,
+      onPanResponderTerminationRequest: () => {
+        return gestureInProgress.current == undefined;
+      },
+      onPanResponderTerminate: (event, gestureState) => {
+        return onGestureRelease(event, gestureState);
+      },
+    });
+  }, []);
+
+
+  const opacity = useRef(new Animated.Value(1));
+
+  const animatedViewProps = props.zoomable ? gestureHandler.panHandlers: null;
+  const image =
+  <Animated.View
+    {...animatedViewProps}
+    style={[{
+      opacity: opacity.current,
+    }, styles.recipeImage]}
+  >
+    <Image
+      ref={imageRef}
+      blurRadius={props.blurredMode ? blurAmount : undefined}
+      source={imageData ? {uri: imageData} : require('../../assets/placeholder.png')}
+      style={[styles.recipeImage, {resizeMode: resizeMode}]} />
+  </Animated.View>;
+
+
+  let overlayImage;
+  if (isDragging) {
+    const animatedStyle = {
+      transform: pinchImagePosition.current.getTranslateTransform(),
+    };
+    animatedStyle.transform.push({
+      scale: scaleValue.current,
+    });
+
+    const imageStyle = [
+      {
+        position: 'absolute',
+        zIndex: 10,
+        // These were saved when the touching started
+        // This is not determed via onLayout since the images can be off screen (pagerview) when onLayout is called
+        width: initialImageSize.current?.width,
+        height: initialImageSize.current?.height,
+        opacity: 1,
+        // opacity: isLoaded ? 1 : 0,
+      },
+      animatedStyle,
+    ];
+
+    overlayImage =
+     <Portal>
+       <View style={{
+         position: 'absolute',
+         top: 0,
+         left: 0,
+         bottom: 0,
+         right: 0,
+       }}>
+         <Animated.View style={{
+           position: 'absolute',
+           top: 0,
+           left: 0,
+           bottom: 0,
+           right: 0,
+
+         }}>
+           <Animated.Image
+             style={imageStyle}
+             source={imageData ? {uri: imageData} : require('../../assets/placeholder.png')}>
+           </Animated.Image>
+         </Animated.View>
+       </View>
+     </Portal>;
+  }
+
   return (
-    <View style={[styles.recipeImage]}>
-      <Image
-        blurRadius={props.blurredMode ? blurAmount : undefined}
-        source={imageData ? {uri: imageData} : require('../../assets/placeholder.png')}
-        style={[styles.recipeImage, {resizeMode: resizeMode}]} >
-      </Image>
-      {requestPending &&
+    <>
+      <View style={[styles.recipeImage]}>
+        {image}
+        {requestPending &&
                 <View style={styles.loadingSpinner}>
                   {/* <Spinner size="giant" /> */}
                 </View>}
 
-    </View>
+      </View>
+      { isDragging && overlayImage }
+    </>
   );
 };
+
 
 const styles = StyleSheet.create({
   loadingSpinner: {
@@ -80,3 +277,41 @@ const styles = StyleSheet.create({
 
   },
 });
+
+
+function calculateScaleFromTouches(touches: NativeTouchEvent[], initialTouches: NativeTouchEvent[]) {
+  const distanceBetweenFingers = getDistanceBetween2Touches(touches);
+  const initialDistanceBetweenFingers = getDistanceBetween2Touches(initialTouches);
+  const scale = calculateScale(distanceBetweenFingers, initialDistanceBetweenFingers);
+  return scale;
+}
+
+function pow2abs(a: number, b: number) {
+  return Math.pow(Math.abs(a - b), 2);
+}
+
+function getDistanceBetween2Touches(touches: NativeTouchEvent[]) {
+  const [a, b] = touches;
+  if (a == null || b == null) {
+    return 0;
+  }
+  return Math.sqrt(pow2abs(a.pageX, b.pageX) + pow2abs(a.pageY, b.pageY));
+}
+
+function getMiddleBetween2Touches(touches: NativeTouchEvent[]) {
+  const [a, b] = touches;
+  if (a == null || b == null) {
+    return {x: 0, y: 0};
+  }
+  return {x: a.pageX, y: a.pageY};
+}
+
+function getDeltaTranslation(position: {x: number, y:number}, initial: {x: number, y:number}) {
+  return {x: position.x - initial.x, y: position.y - initial.y};
+}
+
+const SCALE_MULTIPLIER = 1;
+
+function calculateScale(currentDistance: number, initialDistance: number) {
+  return (currentDistance / initialDistance) * SCALE_MULTIPLIER;
+}
