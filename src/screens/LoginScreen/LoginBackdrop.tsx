f42a4b79d903ad098af7a3405cb9ad0ac4e935e4
steve192 <stefan@sterul.com>
2022-11-27 22:21:39 +0100

    Fix: Make screens with keyboard viewable even when keyboard is opened (#173)
    * Add KeyboardAvoidingView to login screens
    
    * Add keyboardavoiding view to stack navigator

--- src/screens/LoginScreen/LoginBackdrop.tsx
+++ src/screens/LoginScreen/LoginBackdrop.tsx
@@ -1,22 +1,28 @@
 import React, {FunctionComponent} from 'react';
-import {ImageBackground, StyleSheet, View} from 'react-native';
+import {ImageBackground, KeyboardAvoidingView, Platform, StyleSheet, View} from 'react-native';
 import {SafeAreaInsetsContext} from 'react-native-safe-area-context';
+import CentralStyles from '../../styles/CentralStyles';
 
 
 export const LoginBackdrop: FunctionComponent = (props) => {
   return (
-    <ImageBackground
-      style={styles.container}
-      source={require('../../../assets/login-screen.jpg')}>
-      <View style={{backgroundColor: 'rgba(0, 0, 0, 0.45)', position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}>
-        <>
-          <SafeAreaInsetsContext.Consumer>
-            {(insets) => insets && <View style={{paddingTop: insets.top}} />}
-          </SafeAreaInsetsContext.Consumer>
-          {props.children}
-        </>
-      </View>
-    </ImageBackground>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={CentralStyles.fullscreen}
+    >
+      <ImageBackground
+        style={styles.container}
+        source={require('../../../assets/login-screen.jpg')}>
+        <View style={{backgroundColor: 'rgba(0, 0, 0, 0.45)', position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}>
+          <>
+            <SafeAreaInsetsContext.Consumer>
+              {(insets) => insets && <View style={{paddingTop: insets.top}} />}
+            </SafeAreaInsetsContext.Consumer>
+            {props.children}
+          </>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
