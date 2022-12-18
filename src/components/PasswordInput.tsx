8c6e1d1cf6f6e1dc46f226a7d2bb83415d618115
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2022-12-18 14:24:37 +0100

    feat: Add visibility toggle to password inputs (#190)
    * feat: Add visibility toggle to password inputs
    
    * Fix code smell

---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,44 @@
+import React, {useCallback, useMemo, useState} from 'react';
+import {TextInput} from 'react-native-paper';
+
+interface Props {
+  password: string,
+  setPassword: (newPassword: string) => void,
+  label: string,
+  error?: boolean,
+}
+
+const ICON_INPUT_HIDDEN = 'eye-off';
+const ICON_INPUT_VISIBLE = 'eye';
+
+export const PasswordInput = (props: Props) => {
+  const [inputHidden, setInputHidden] = useState<boolean>(true);
+
+  const toggleInputHidden = useCallback(
+      () => setInputHidden(!inputHidden),
+      [inputHidden],
+  );
+
+  const toggleHiddenIcon = useMemo(
+      () => (
+        <TextInput.Icon
+          icon={inputHidden ? ICON_INPUT_HIDDEN : ICON_INPUT_VISIBLE}
+          onPress={toggleInputHidden}
+          forceTextInputFocus={false}
+        />
+      ), [inputHidden],
+  );
+
+  return (
+    <TextInput
+      dense={true}
+      mode="flat"
+      value={props.password}
+      onChangeText={props.setPassword}
+      label={props.label}
+      error={props.error}
+      secureTextEntry={inputHidden}
+      right={toggleHiddenIcon}
+    />
+  );
+};
