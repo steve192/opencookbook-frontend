fc648156ef168d6fd46fc970bdf0dfff08241464
steve192 <stefan@sterul.com>
2023-01-09 22:41:47 +0100

    Use recipe group selection popup

--- src/screens/RecipeListScreen.tsx
+++ src/screens/RecipeListScreen.tsx
@@ -5,9 +5,11 @@ import React, {useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {Appbar, FAB, Surface, useTheme} from 'react-native-paper';
 import {RecipeList} from '../components/RecipeList';
+import {Option, SelectionPopupModal} from '../components/SelectionPopupModal';
 import {Recipe} from '../dao/RestAPI';
 import {MainNavigationProps, OverviewNavigationProps, RecipeScreenNavigation} from '../navigation/NavigationRoutes';
-import {useAppSelector} from '../redux/hooks';
+import {updateRecipe} from '../redux/features/recipesSlice';
+import {useAppDispatch, useAppSelector} from '../redux/hooks';
 import CentralStyles from '../styles/CentralStyles';
 
 
@@ -26,11 +28,15 @@ const RecipeListScreen = (props: Props) => {
 
   const [fabOpen, setFabOpen] = useState(false);
 
-  const shownRecipeGroup = useAppSelector((state) => state.recipes.recipeGroups.filter((recipeGroup) => recipeGroup.id == props.route.params?.shownRecipeGroupId)[0]);
 
+  const allRecipeGroups = useAppSelector((state) => state.recipes.recipeGroups);
+  const allRecipes = useAppSelector((state) => state.recipes.recipes);
+  const shownRecipeGroup = useAppSelector((state) => state.recipes.recipeGroups.filter((recipeGroup) => recipeGroup.id == props.route.params?.shownRecipeGroupId)[0]);
 
   const [selectedRecipes, setSelectedRecipes] = useState(new Set<number>());
   const [multiSelectionModeActive, setMultiSelectionModeActive] = useState(false);
+
+  const [recipeGroupSelectionOpened, setRecipeGroupSelectionOpened] = useState(false);
 
   useEffect(() => {
     const adjustActionbar = () => {
@@ -41,7 +47,7 @@ const RecipeListScreen = (props: Props) => {
             <Appbar.Action
               icon="group"
               color={theme.colors.textOnPrimary}
-              onPress={() => null} />
+              onPress={() => setRecipeGroupSelectionOpened(true)} />
           ),
         });
       } else {
@@ -63,9 +69,16 @@ const RecipeListScreen = (props: Props) => {
         }
       }
     };
+    adjustActionbar();
     return props.navigation.addListener('focus', adjustActionbar);
   }, [props.navigation, shownRecipeGroup, multiSelectionModeActive, selectedRecipes]);
 
+  const getRecipeGroupOptions = () => {
+    // The key for "no group selected". Can be anything that will never exist in the real ids
+    let groups: Option[] = [{key: 'none', value: 'No group'}];
+    groups = [...groups, ...allRecipeGroups.map((group) => ({key: group.id ? group.id.toString() : '', value: group.title}))];
+    return groups;
+  };
 
   const openRecipe = (recipe: Recipe) => {
     if (recipe.id) {
@@ -73,6 +86,22 @@ const RecipeListScreen = (props: Props) => {
         recipeId: recipe.id,
       });
     }
+  };
+
+  const onMoveSelectedRecipesToGroup = (selectedOption: Option) => {
+    // TODO: Move recipe objects to selected recipes instead of ids only
+    const recipesToMove = allRecipes.filter((recipe) => selectedRecipes.has(recipe.id!) );
+    recipesToMove.forEach((recipe) => {
+      const recipeDataCopy = {...recipe};
+      if (selectedOption.key === '') {
+        recipeDataCopy.recipeGroups = [];
+      } else {
+        // @ts-ignore
+        recipeDataCopy.recipeGroups = [{id: +selectedOption.key}];
+      }
+      console.log('updating recipe');
+      useAppDispatch(updateRecipe(recipeDataCopy));
+    });
   };
 
   const onRecipeSelected = (selectedRecipe: number) => {
@@ -126,6 +155,11 @@ const RecipeListScreen = (props: Props) => {
           ]}
         />
       </Surface>
+      {recipeGroupSelectionOpened && <SelectionPopupModal
+        modalVisible={recipeGroupSelectionOpened}
+        options={getRecipeGroupOptions()}
+        onClose={() => setRecipeGroupSelectionOpened(false)}
+        onSelection={onMoveSelectedRecipesToGroup} /> }
     </>
   );
 };
