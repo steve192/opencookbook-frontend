e7e67621f11cf31784770f537906b0fa57ba55ff
steve192 <stefan@sterul.com>
2023-01-10 21:36:13 +0100

    Fix sonar issues

--- src/components/SelectionPopupModal.tsx
+++ src/components/SelectionPopupModal.tsx
@@ -1,10 +1,11 @@
 import {HeaderHeightContext} from '@react-navigation/elements';
 import React, {useRef, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {Modal, Pressable, StyleSheet, View} from 'react-native';
+import {Modal, Pressable, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import {Divider, List, Surface, TextInput} from 'react-native-paper';
 import Spacer from 'react-spacer';
+import {modalStyles} from '../styles/CentralStyles';
 
 
 export interface Option {
@@ -64,7 +65,7 @@ export const SelectionPopupModal = (props: Props) => {
     return listItems;
   };
 
-  return <View style={styles.centeredView}>
+  return <View style={modalStyles.centeredView}>
     <Modal
       animationType="slide"
       transparent={true}
@@ -74,13 +75,13 @@ export const SelectionPopupModal = (props: Props) => {
     >
       <Pressable
         onPress={() => props.onClose()}
-        style={styles.modalBackdrop}>
+        style={modalStyles.modalBackdrop}>
         <HeaderHeightContext.Consumer>
           {(headerHeight) => headerHeight &&
             <>
-              <View style={styles.centeredView}>
+              <View style={modalStyles.centeredView}>
                 {/* headerHeight / 2 is a workaround. Calculate the real header height (header height is navigation bar + safe area, instead of only navigation bar)*/}
-                <Surface style={[{flex: 1, marginTop: (headerHeight / 2), width: '100%'}, styles.modalView]}>
+                <Surface style={[{flex: 1, marginTop: (headerHeight / 2), width: '100%'}, modalStyles.modalView]}>
                   <View
                     style={{flexDirection: 'row', alignContent: 'center'}}>
                     <TextInput placeholder={props.placeholder} ref={modalInputRef} onChangeText={onSearchInputChange} style={{flex: 1}} value={value} />
@@ -104,39 +105,3 @@ export const SelectionPopupModal = (props: Props) => {
 };
 
 
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-    width: '100%',
-  },
-  modalBackdrop: {
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-    backgroundColor: 'rgba(0,0,0,0.5)',
-  },
-  modalView: {
-    width: '90%',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-    maxWidth: 600,
-    maxHeight: 800,
-    flex: 1,
-    marginBottom: 10,
-    padding: 10,
-    marginHorizontal: 10,
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
-  },
-});
